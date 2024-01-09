82b6b483848dea6ed3ec0d76b1fdd37d54126a0e
yhd0208 <2465254993@qq.com>
2024-01-09 10:10:35 +0800

    修改日期

--- themes/hexo/components/LatestPostsGroup.js
+++ themes/hexo/components/LatestPostsGroup.js
@@ -52,7 +52,7 @@ const LatestPostsGroup = ({ latestPosts, siteInfo }) => {
                     >
                         <div>
                             <div className='line-clamp-2 menu-link'>{post.title}</div>
-                            <div className="text-gray-500">{post.lastEditedDay}</div>
+                            <div className="text-gray-500">{post.publishDay}</div>
                         </div>
                     </div>
 
